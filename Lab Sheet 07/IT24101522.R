01a18e7e069c10544b7ccc030d4c35c3f56c0345
IT24101522 <it24101522@my.sliit.lk>
2025-09-16 11:46:38 +0530

    upload_lab07

---
+++ Lab Sheet 07/IT24101522.R
@@ -0,0 +1,18 @@
+setwd("C:\\Users\\ASUS\\OneDrive - Sri Lanka Institute of Information Technology\\Desktop\\IT24101522")
+
+--(1)
+
+punif(25, min=0, max=40) - punif(10, min=0, max=40)
+
+--(2)
+pexp(2, rate=1/3)
+
+
+--(3)
+--i 
+1 - pnorm(130, mean=100, sd=15)
+
+
+--ii
+qnorm(0.95, mean=100, sd=15)
+
